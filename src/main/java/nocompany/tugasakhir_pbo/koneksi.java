ff9cd8d0ce78876f1941ffb1238b55245def2158
bilal <mb336450@gmail.com>
2024-06-12 14:58:39 +0700

    fix remove connection in git ignore

--- src/main/java/nocompany/tugasakhir_pbo/koneksi.java
+++ src/main/java/nocompany/tugasakhir_pbo/koneksi.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class koneksi {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/kasir-java";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "";
+    private static final String USERNAME = "bilal";
+    private static final String PASSWORD = "bilal123";
 
     private static Connection connection;
 
